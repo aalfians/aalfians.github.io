e43105959866d8356fd12a72dbf74adc354698c5
aalfians <42733998+aalfians@users.noreply.github.com>
2022-12-08 09:05:52 +0700

    Update app.js

--- video_rec/app.js
+++ video_rec/app.js
@@ -15,15 +15,7 @@ function preload() {
 
 function setup() {
   createCanvas(640, 480);
-  var constraints = {
-  audio: false,
-  video: {
-     facingMode: {
-        exact: "environment"
-      }
-    }    
-  };
-  video = createCapture(constraints);
+  video = createCapture(VIDEO);
   video.size(640, 480);
   console.log('video element is created');
   video.elt.addEventListener('loadeddata', function() {
@@ -58,7 +50,7 @@ function drawLabel(object) {
   fill('orange');
   textSize(24);
   text(object.label, object.x + 10, object.y + 24);
-  text(nf(object.confidence*100,2,2), object.x + 10, object.y + 48);
+  text(nf(object.confidence*100,2,2),"%", object.x + 10, object.y + 48);
 }
 
 // callback function. it is called when object is detected
